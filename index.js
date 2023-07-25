25c06c5a105aee90f1a0766c448b976abfcc177e
tykozidane2015 <tyko.nutech@gmail.com>
2023-07-25 13:54:54 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Server } = require("socket.io");
+
+const io = new Server({ cors: "http://127.0.0.1:5173/" });
+let onlineUsers = [];
+io.on("connection", (socket) => {
+  console.log("New Connection", socket.id)
+
+  //listen to a connection
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some((user) => user.userId === userId) &&
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      }) ;
+
+      console.log("onlineUsers", onlineUsers);
+  
+      io.emit("getOnlineUsers", onlineUsers);
+    });
+
+    socket.on("sendMessage", (message) => {
+      const user = onlineUsers.find((user) => user.userId === message.recipientId);
+      console.log("type message", message.typeMessage);
+      if(user){
+        io.to(user.socketId).emit("getMessage", message);
+        io.to(user.socketId).emit("getNotification", {
+          typeMessage: message.typeMessage,
+          chatId: message.chatId,
+          senderId: message.senderId,
+          isRead: false,
+          date: new Date(),
+        })
+      }
+    });
+
+    socket.on("disconnect", () => {
+      onlineUsers= onlineUsers.filter((user) => user.socketId !== socket.id)
+
+      io.emit("getOnlineUsers", onlineUsers);
+    })
+});
+
+io.listen(3001);
